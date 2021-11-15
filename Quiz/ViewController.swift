522324d40ee08db24e74bab3602ae13d24f9d18f
giorgirogava <giorgirogava01ai@gmail.com>
2021-11-15 21:39:09 +0400

    - feat: add Quiz questions and answers

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
 
+    @IBAction func startGame() {
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "game") as! GameViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+        
+    }
 }
 
